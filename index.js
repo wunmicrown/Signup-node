eb41cd457a4f6ffbcb00b415271900295c061dc6
wunmicrown <olayiwolapamilerin34@gmail.com>
2023-12-29 06:55:20 +0100

    added listening port my app to be working fin

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ const ejs = require("ejs")
 
 app.set("view engine","ejs")
 
+app.listen(port,(error)=>{
+    if (error) {
+        console.log(`I can't run on port ${port}`);
+    } else {
+        console.log(`I am runing on port ${port}`);
+    }
+})
+
