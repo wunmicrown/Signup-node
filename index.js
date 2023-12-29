dd1bd5a27c93de0f373332a55a5b63fbe2935807
wunmicrown <olayiwolapamilerin34@gmail.com>
2023-12-29 06:54:14 +0100

    import ejs to my folder

--- index.js
+++ index.js
@@ -7,26 +7,3 @@ const ejs = require("ejs")
 
 app.set("view engine","ejs")
 
-app.listen(port,(error)=>{
-    if (error) {
-        console.log(`I can't run on port ${port}`);
-    } else {
-        console.log(`I am runing on port ${port}`);
-    }
-})
-
-
-app.get("/",(req,res)=>{
-    // res.send("hello node class");
-    res.render("index", {car:"Toyota Camry"})
-
-})
-app.get("/Welcome", (req, res)=>{
-    res.sendFile(__dirname + "/index.html")
-})
-app.get("/login", (req, res)=>{
-    res.sendFile(__dirname + "/login.html")
-})
-app.get("/signup", (req, res)=>{
-    res.sendFile(__dirname + "/signup.html")
-})
